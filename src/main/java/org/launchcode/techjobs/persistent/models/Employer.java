fba344dbe8ddfe01447e136a56e589561f870fa6
sjesus <s.jesus10@yahoo.com>
2023-06-01 19:44:10 -0500

    Added repositories

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotEmpty
+    @Length(max = 250)
+    private String location;
 
+    public Employer() {};
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
