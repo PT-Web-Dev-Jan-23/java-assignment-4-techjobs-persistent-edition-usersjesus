10f5056a6555356db88a1469d5a478b5cca85af8
sjesus <s.jesus10@yahoo.com>
2023-06-06 21:31:30 -0500

    Uncommented task three

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -3,15 +3,23 @@ package org.launchcode.techjobs.persistent.models;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
     @NotEmpty
     @Length(max = 250)
     private String location;
+
+    @OneToMany
+    @JoinColumn(name = "item_id")
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer() {};
 
